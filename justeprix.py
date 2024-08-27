48932cc5de6c8cc42a7ed12a01d1b2df718e0052
Carla <carla.buchmann@eleve.isep.fr>
2024-08-27 14:53:43 +0200

    petite modification

--- justeprix.py
+++ justeprix.py
@@ -3,18 +3,21 @@ print("Trouver le prix de l'objet X..")
 cible = (random.randint(1,10))
 #print(f"cible={cible}")
 
-# récupération de la proposition du joueur
-essai = int(input("Votre proposition entre 1 et 10 : "))
+for i in range(5):
+    # récupération de la proposition du joueur
+    essai = int(input("Votre proposition entre 1 et 10 : "))
 
-if essai < 1 or essai > 10 :
-    print("Erreur, il faut saisir une valeur entre 1 et 10...")
-    exit()
+    if essai < 1 or essai > 10 :
+        print("Erreur, il faut saisir une valeur entre 1 et 10...")
+        exit()
 
 
-#else et elif sont contextuels, ils sous entendent que 'if cible == essai: ' est faux
-if cible == essai:
-    print("Bravo !!!")
-elif cible < essai :
-    print("Trop élevé")
-else:
-    print("Trop peu")
+    #else et elif sont contextuels, ils sous entendent que 'if cible == essai: ' est faux
+    if cible == essai:
+        print("Bravo !!!")
+        break
+    elif cible < essai :
+        print("Trop élevé")
+    else:
+        print("Trop peu")
+print("Fin du jeu...")
